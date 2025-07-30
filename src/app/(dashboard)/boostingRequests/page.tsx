7fa55397ceec9b620e09bc7bac6e4197d304ca8a
almas hossain <almashossain7384@gmail.com>
2025-07-30 14:56:24 +0600

    boosting control section delete modal and accept modal design done

--- src/app/(dashboard)/boostingRequests/page.tsx
+++ src/app/(dashboard)/boostingRequests/page.tsx
@@ -15,11 +15,9 @@ import {
     TableRow,
 } from "@/components/ui/table"
 import CustomModal from '@/components/modal/customModal'
-import UserDetailsKycRequest from '@/components/user/user-details-kyc-request'
-import UserDetailsAccepted from '@/components/user/user-details-accept'
-import UserRejectionKycRequest from '@/components/user/user-rejection-kyc-request'
-import KycRejectedDeclined from '@/components/user/kyc-rejected-declined'
 import Image from 'next/image'
+import BoostRequestAccepted from '@/components/boostControl/boost_request_accepted'
+import RejectBoosting from '@/components/boostControl/reject_boosting'
 
 
 const BoostingRequests = () => {
@@ -29,6 +27,8 @@ const BoostingRequests = () => {
     const [issOpen, setIsOpen] = useState(false)
     const [isOpenRej, setIsOpenRej] = useState(false)
     const [isOpenRejTwo, setIsOpenRejTwo] = useState(false)
+    const [bookingReqModal, setBookingReqModal] = useState(false)
+    const [bookingReqModalRej, setBookingReqModalRej] = useState(false)
 
     const handleNavigate = () => {
         try {
@@ -188,8 +188,10 @@ const BoostingRequests = () => {
                                                 <path d="M18.5 15.8C17.632 15.8 16.7996 16.1371 16.1858 16.7373C15.5721 17.3374 15.2273 18.1513 15.2273 19C15.2273 19.8487 15.5721 20.6626 16.1858 21.2627C16.7996 21.8629 17.632 22.2 18.5 22.2C19.368 22.2 20.2004 21.8629 20.8142 21.2627C21.4279 20.6626 21.7727 19.8487 21.7727 19C21.7727 18.1513 21.4279 17.3374 20.8142 16.7373C20.2004 16.1371 19.368 15.8 18.5 15.8ZM18.5 24.3333C17.0534 24.3333 15.666 23.7714 14.6431 22.7712C13.6201 21.771 13.0455 20.4145 13.0455 19C13.0455 17.5855 13.6201 16.229 14.6431 15.2288C15.666 14.2286 17.0534 13.6667 18.5 13.6667C19.9466 13.6667 21.334 14.2286 22.3569 15.2288C23.3799 16.229 23.9545 17.5855 23.9545 19C23.9545 20.4145 23.3799 21.771 22.3569 22.7712C21.334 23.7714 19.9466 24.3333 18.5 24.3333ZM18.5 11C13.0455 11 8.38727 14.3173 6.5 19C8.38727 23.6827 13.0455 27 18.5 27C23.9545 27 28.6127 23.6827 30.5 19C28.6127 14.3173 23.9545 11 18.5 11Z" fill="#F96D10" />
                                             </svg>
                                         </button>
+
+
                                         <button
-                                            onClick={() => setIsOpen(!issOpen)}
+                                            onClick={() => setBookingReqModal(!bookingReqModal)}
                                             className="cursor-pointer">
                                             <svg width="37" height="38" viewBox="0 0 37 38" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <rect y="0.5" width="37" height="37" rx="5" fill="#D8FFD8" />
@@ -198,7 +200,7 @@ const BoostingRequests = () => {
 
                                         </button>
                                         <button
-                                            onClick={() => setIsOpenRej(!isOpenRej)}
+                                            onClick={() => setBookingReqModalRej(!bookingReqModalRej)}
                                             className="cursor-pointer">
                                             <svg width="34" height="38" viewBox="0 0 34 38" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <rect width="34" height="38" rx="6" fill="#FFE8E8" />
@@ -213,6 +215,30 @@ const BoostingRequests = () => {
                 </Table>
             </div>
 
+
+            {/* modal component(BOOSTING_ACCEPTED) */}
+            <CustomModal
+                open={bookingReqModal}
+                setIsOpen={setBookingReqModal}
+                className={"p-2 max-h-[0vh]"}
+                maxWidth={"!max-w-[30vw]"}
+            >
+                <BoostRequestAccepted
+                    open={bookingReqModal}
+                    setIsOpen={setBookingReqModal}
+                />
+            </CustomModal>
+
+
+            {/* modal component(BOOSTING_REJECTED) */}
+            <CustomModal
+                open={bookingReqModalRej}
+                setIsOpen={setBookingReqModalRej}
+                className={"p-2 max-h-[0vh]"}
+                maxWidth={"!max-w-[30vw]"}
+            >
+                <RejectBoosting />
+            </CustomModal>
         </div >
     )
 }
